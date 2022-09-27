3a6765cfb367c8513e8dd3d02c49a01f556d8c78
冯汐辰 <10545135+feng-xichen@user.noreply.gitee.com>
2022-09-27 15:39:09 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		{{vuex_name}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				auth:{
+					email:'test@a.com',
+					password:123123
+				}
+			}
+		},
+		async onLoad() {
+			// const res = await this.$u.api.login(this.auth);
+			// console.log(res);
+			console.log(this.vuex_name);
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
